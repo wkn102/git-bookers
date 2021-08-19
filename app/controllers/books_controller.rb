ca9d4292758a6f37c7b261999b8dc2db3154dc40
wkn102 <lino.libra1002@gmail.com>
2021-08-19 07:55:33 +0000

    Initial Commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,46 @@
+class BooksController < ApplicationController
+  def index
+     @books = Book.all
+     @book = Book.new
+  end
+
+  def show
+     @book = Book.find(params[:id])
+  end
+
+  def create
+     @book = Book.new(book_params)
+     @books = Book.all
+   if @book.save
+    redirect_to book_path(@book), notice: 'successfully'
+   else
+     render :index
+   end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @books =Book.all
+  if @book.update(book_params)
+    redirect_to book_path(@book), notice: 'successfully'
+  else
+    render:edit
+   end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
