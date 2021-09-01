985ddf3e2cc87d6ab8412098e402eea2ed71f756
wkn102 <lino.libra1002@gmail.com>
2021-09-01 05:44:36 +0000

    Submit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,7 @@
 class BooksController < ApplicationController
+
   def index
+     @books = Book.all
      @books = Book.all
      @book = Book.new
   end
@@ -10,10 +12,10 @@ class BooksController < ApplicationController
 
   def create
      @book = Book.new(book_params)
-     @books = Book.all
    if @book.save
     redirect_to book_path(@book), notice: 'successfully'
    else
+     @books = Book.all
      render :index
    end
   end
@@ -25,9 +27,9 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     @books =Book.all
-  if @book.update(book_params)
+   if @book.update(book_params)
     redirect_to book_path(@book), notice: 'successfully'
-  else
+   else
     render:edit
    end
   end
